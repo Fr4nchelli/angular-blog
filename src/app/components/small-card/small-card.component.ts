54d8e457d331bda565fba81629f72ed3fc20d0cd
Fr4nchelli <Viniciusfrancelli@gmail.com>
2023-10-13 15:22:14 -0300

    conteudo criado

--- src/app/components/small-card/small-card.component.ts
+++ src/app/components/small-card/small-card.component.ts
@@ -11,6 +11,8 @@ export class SmallCardComponent implements OnInit {
   @Input()
   cardTitle=""
   constructor() { }
+  @Input()
+  Id:string="0"
 
   ngOnInit(): void {
   }
---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,26 @@
+export const dataFake = [
+  {
+    "id":"1",
+    "title": "Pokemon lendário é visto nas montanhas!",
+    "description": "Lorota news investiga suposta aparição do pokemon lendário RAYQUAZA, segundo moradores da região o pokemon foi visto na madrugada de ontem, por volta de 1h da manhã",
+    "photoCover": "https://i.pinimg.com/474x/c2/1f/1f/c21f1fabfad30bfcf649f1828783c2ce.jpg"
+  },
+  {
+    "id":"2",
+    "title": "Equipe rocket faz vitima na cidade de pallet!",
+    "description": "Equipe rocket faz um novo furto na cidade de palet, morador que foi atacado nos da detalhes do ocorrido, 'me atacaram porque eu dormi enquanto eles se apresentavam!'",
+    "photoCover": "https://kanto.legiaodosherois.com.br/w760-h398-cfill/wp-content/uploads/2016/08/legiao_FsBbOYCqoj0PvEelRHfWiX2aZKyu1MNtQ3p6S_JAdT.jpg.webp"
+  },
+  {
+    "id":"3",
+    "title": "10º edição do concurso de beleza começa semana que vem!",
+    "description": "O concurso mais aguardade de todos os tempos chega na cidade, confira os requisitos para cadastrar seu pokemon!",
+    "photoCover": "https://blogger.googleusercontent.com/img/a/AVvXsEiCb5ZupKDIx_OKSy51JzkMPxJQVSNZ2BXYOFA5UgBxVNa_7g9Go0PNfg8x9teBj4pFU3IXqGXIzZogS8_7H9GtVd2cFwrHg-UVSx54GSpd_RpNVihfGfhbJ8Xs065U_hHpA1mrCOypVciWBCB_OdDx9JLfiFR-7cnuoVQURXPcggn-Pu5D15BlMPSBCQ=w400-h283"
+  },
+  {
+    "id":"4",
+    "title": "Palesta com o professor carvalho, qual o melhor inicial?",
+    "description": "Nesta Quinta-feira acontecerá uma palestra no centro pokemon de viridian com o professor carvalho, onde ele dará dicas de qual o melhor pokemon para iniciar a sua jornada!",
+    "photoCover": "https://1.bp.blogspot.com/-gCtfexqeoSs/XaZOvMoOrvI/AAAAAAAAU1U/jmwLfO43MNQdRX4pCdJ4FksNSuXjDxxDQCLcBGAsYHQ/w1200-h630-p-k-no-nu/Professor%2Boak%2Bcarvalho%2Bbiografia.jpg"
+  }
+]
